b9753b54ac60e1773278d9253f27a0f10158583f
Dinesh <dinesh@DESKTOP>
2024-06-16 18:49:00 +0530

    add main.c and helper.c

---
+++ helper.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include "helper.h"
+
+void helper_function() {
+    printf("This is a helper function.\n");
+}
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "helper.h"
+
+int main() {
+    printf("Hello, World!\n");
+    helper_function();
+    return 0;
+}
+
